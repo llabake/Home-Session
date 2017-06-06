edb96a0293fc891a01132aa14a4c5fbecb9c9dd2
llabake <lemboyelabake@gmail.com>
2017-06-06 06:55:10 +0100

    Add test suite for user input of 0 and negative number

---
+++ app/library.js
@@ -0,0 +1,33 @@
+var checkPrimes = function (num) {
+    if (typeof (num) != "number") {
+        return [];
+    }
+    else if (num<=0){
+        return [];
+    }
+    else {
+        var temp = [];
+        if (num < 2) {
+            return false;
+        }
+        for (var i = 2; i < num; i++) {
+            if (num % i === 0) {
+                temp.push(i);
+            }
+        }
+        return temp.length === 0;
+    }
+
+};
+
+var getPrimes = function (number) {
+    var primes = [];
+    for (var i = 1; i <= number; i++) {
+        if (checkPrimes(i)) {
+            primes.push(i);
+        }
+    }
+    return primes;
+};
+
+module.exports = getPrimes;
